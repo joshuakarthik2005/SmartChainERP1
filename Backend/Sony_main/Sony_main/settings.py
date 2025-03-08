8afcce3f3092d1a14c590b757c3823063ab3fdfb
joshuakarthik2005 <joshuakarthik2005@gmail.com>
2025-03-08 21:36:43 +0530

    gooooooooooooo

--- Backend/Sony_main/Sony_main/settings.py
+++ Backend/Sony_main/Sony_main/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'staticfiles'))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
